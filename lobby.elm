f83498df934448b02ca018a6a40f0dc7578dfb5b
mario2904 <orbegoso2904@gmail.com>
2016-07-11 16:51:58 -0400

    Made basic page that connects to the WebSocketServer and show the random player id assigned at the moment of connection and a list of all players (including the user himself) that are active/connected to the WebSocketServer. The list updates every time a new player enters or a player disconnects.

---
+++ lobby.elm
@@ -0,0 +1,101 @@
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import WebSocket
+import Route.QueryString exposing (..)
+
+
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+briscaServer: String
+briscaServer =
+  "wss://brisca-server.herokuapp.com"
+
+
+-- MODEL
+
+
+
+type alias Model =
+  { players: List String
+  , playerId: String
+  }
+
+
+
+init: (Model, Cmd Msg)
+init =
+  (Model [] "", Cmd.none)
+
+
+-- UPDATE
+
+
+
+type Msg = Send
+  | Input String
+  | NewMessage String
+
+
+update: Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    NewMessage str ->   -- PARSE QUERYSTRING AND RESOLVE
+      let
+        qs = parse str
+        cmd = one string "cmd" qs |> Maybe.withDefault ""  -- handle Error
+      in
+        if cmd == "updatePlayers" then
+          let
+            newPlayers = all "players" qs
+          in
+            ({model | players = newPlayers}, Cmd.none)
+        else if cmd == "playerId" then
+          let
+            newPlayerId = one string "player" qs |> Maybe.withDefault "Guest"-- handle Error
+          in
+            ({model | playerId = newPlayerId}, Cmd.none)
+        else
+          (model, Cmd.none)
+
+    _ ->
+      (model, Cmd.none)
+
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  WebSocket.listen briscaServer NewMessage
+
+
+-- VIEW
+
+
+
+view: Model -> Html Msg
+view model =
+  div[]
+    [ h1 [] [text model.playerId]
+    , div [] (viewList model)
+    ]
+
+
+-- VIEW PLAYERS LIST
+
+
+viewList: Model -> List (Html Msg)
+viewList model =
+  List.map (\player -> div [][text player]) model.players
